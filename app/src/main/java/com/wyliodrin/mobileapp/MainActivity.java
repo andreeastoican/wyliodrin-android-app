cc25d9fc3df843cb6a732b56c4cb053eac14615f
Andreea <andreea@andreea-X550CL.(none)>
2014-12-17 17:20:37 +0200

    drawer menu

--- app/src/main/java/com/wyliodrin/mobileapp/MainActivity.java
+++ app/src/main/java/com/wyliodrin/mobileapp/MainActivity.java
@@ -1,97 +1,42 @@
 package com.wyliodrin.mobileapp;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-
-import com.wyliodrin.mobileapp.api.WylioBoard;
-import com.wyliodrin.mobileapp.widgets.BarGraphWidget;
-import com.wyliodrin.mobileapp.widgets.StepGraphWidget;
-import com.wyliodrin.mobileapp.widgets.Widget;
-
-import java.util.List;
-import java.util.Random;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    Thread t;
-    int x = 0;
-    Random rand = new Random();
-
-    List<Widget> widgets;
+    private String[] widgetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_drawer);
 
-        // Simple Message
-        final WylioBoard board = new WylioBoard("250a7843-9eda-4a92-942d-dba0dfe3b0a0b836cb66-0ad1-46d6-b6ba-458781a38ca08ced6536-0a8a-49b0-bbf3-b072631d0ccf", "andreea.stoican.5_random@wyliodrin.com");
+        widgetTitles = new String[] {"Moon", "Sun"};
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        //final WylioBoard board = new WylioBoard("b883db27-a072-4481-aff2-2e5297d781c80a6febc0-6263-4118-9eeb-2fbb0530585b46a813ed-7eda-49b4-be5e-4a319c6c0da13a155792-fc2e-413f-82d9-9728ca5b829a");
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_elem, widgetTitles));
 
-        Button sendButtonOn = (Button) findViewById(R.id.buttonon);
-        sendButtonOn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                board.sendMessage("ledon", "ledon");
-            }
-        });
+        Fragment fragment = new MainFragment();
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment).commit();
 
-        Button sendButtonOff = (Button) findViewById(R.id.buttonoff);
-        sendButtonOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                board.sendMessage("ledoff", "ledoff");
-            }
-        });
-
-        Button realTimeGraphic = (Button) findViewById(R.id.realtTimeGraphButton);
-        realTimeGraphic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addGraph();
-            }
-        });
-
-    }
-
-    public void addGraph() {
-
-        //final GraphWidget graph = new GraphWidget(this);
-        //final BarGraphWidget graph = new BarGraphWidget(this);
-        final StepGraphWidget graph = new StepGraphWidget(this);
-
-        LinearLayout layout = (LinearLayout) findViewById(R.id.graphContainer);
-        layout.getLayoutParams().height = 300;
-        layout.getLayoutParams().width = 200;
-        layout.requestLayout();
-        layout.addView(graph);
-
-        t = new Thread() {
-            @Override
-            public void run() {
-                super.run();
-
-                while(true) {
-                    x++;
-                    double y = rand.nextDouble();
-
-                    graph.addPoint(x, y);
-
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        t.start();
+            mDrawerLayout.closeDrawer(mDrawerList);
+        }
 
     }
 
